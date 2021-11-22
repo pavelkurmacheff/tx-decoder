a16ed83731a348708068f87195dd5535ed70658f
shoom3301 <shoom3301@gmail.com>
2021-11-22 13:11:41 +0600

    fix: fix getReturnAmountFromLogs

--- src/helpers/dest-amount.helper.ts
+++ src/helpers/dest-amount.helper.ts
@@ -13,7 +13,7 @@ export function getDestAmountViaEstimation(
 }
 
 export function getReturnAmountFromLogs(receipt: TransactionReceipt): BigNumber {
-    const walletAddress = receipt.from;
+    const walletAddress = receipt.from.toLowerCase();
     const transferTopic = '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef';
     const withdrawalTopic = '0x7fcf532c15f0a6db0bd6d0e038bea71d30d808c7d98cb3bf7268a95bf5081b65';
 
@@ -21,7 +21,7 @@ export function getReturnAmountFromLogs(receipt: TransactionReceipt): BigNumber 
         const eventTopic = log.topics[0];
         const receiverAddress = ('0x' + (log.topics[2] || '').slice(26));
 
-        return eventTopic === transferTopic && receiverAddress === walletAddress;
+        return eventTopic === transferTopic && receiverAddress.toLowerCase() === walletAddress;
     });
 
     const withdrawalEvent = receipt.logs.find(log => {
