3654284832deb7b0f0200fa1ef991619c3097e11
Alexandr Kazachenko <shoom3301@gmail.com>
2021-12-30 15:29:00 +0600

    feat: add example app (#3)
    * feat: add example app
    
    * fix: remove specs file for example
    
    * fix: add example app to ignore for jest
    
    * chore: remove extra icons and fonts
    
    * feat: add validation for inputs
    
    * feat: demo app
    
    * chore: fix demo app base href
    
    Co-authored-by: limitofzero <limitofzero@gmail.com>

--- app-example/src/environments/environment.prod.ts
+++ app-example/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    tokenListApiUrl: 'https://tokens.1inch.io/v1.1/1',
-    tokenPriceApiUrl: 'https://token-prices.1inch.io/v1.1/1',
+    tokenListApiUrl: 'https://tokens.1inch.io/v1.1',
+    tokenPriceApiUrl: 'https://token-prices.1inch.io/v1.1',
 };
