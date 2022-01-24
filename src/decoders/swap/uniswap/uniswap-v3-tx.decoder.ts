e1e82d78088a0628f0813e6beb12b26b9758c885
Nick Kozlov <22479658+EnoRage@users.noreply.github.com>
2022-01-24 14:10:42 +0300

    Update uniswap-v3-tx.decoder.ts

--- src/decoders/swap/uniswap/uniswap-v3-tx.decoder.ts
+++ src/decoders/swap/uniswap/uniswap-v3-tx.decoder.ts
@@ -4,6 +4,7 @@ import { BigNumber } from '@ethersproject/bignumber';
 import { SwapTxDecoded } from '../../../model/swap-tx.model';
 import { TransactionReceipt } from '@ethersproject/abstract-provider';
 
+// todo: what is that? why is it here?
 export interface UniswapV3TxItemData {
     amount: string;
     minReturn: string;
@@ -13,6 +14,7 @@ export interface UniswapV3TxItemData {
 export class UniswapV3TxDecoder implements TxDecoder<UniswapV3TxItemData> {
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
+    // todo: what is that? why is it here?
     readonly decodeInfo: DecodeInfo;
 
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
@@ -25,6 +27,7 @@ export class UniswapV3TxDecoder implements TxDecoder<UniswapV3TxItemData> {
 
     async decodeByConfig(txConfig: Transaction): Promise<SwapTxDecoded> {
         console.log(txConfig);
+        // console.log(callResult);
         return {
             dstAmount: BigNumber.from('0'),
             dstToken: {
@@ -95,4 +98,87 @@ export class UniswapV3TxDecoder implements TxDecoder<UniswapV3TxItemData> {
     //     dstAmount
     // }, this.resources);
     // }
+}
+
+
+export enum uniswapTxType {
+    exactIn = 'exactIn',
+    exactOut = 'exactOut',
+    unkown = 'unkown',
+}
+
+export interface MulticallParam {
+    name: string;
+    value: string;
+    type: string;
+}
+
+export interface DecoderResult {
+    name: string;
+    params: any;
+}
+
+export interface methodDetails {
+    name: string;
+    type: uniswapTxType;
+}
+
+export const uniswapV3MethodsMap = {
+    '0x472b43f3': {
+        name: 'swapExactTokensForTokens',
+        type: 'exactIn',
+    },
+    '0x42712a67': {
+        name: 'swapTokensForExactTokens',
+
+        type: 'exactOut',
+    },
+    '0x04e45aaf': {
+        name: 'exactInputSingle',
+        type: 'exactIn',
+    },
+    '0x5023b4df': {
+        name: 'exactOutputSingle',
+        type: 'exactOut',
+    },
+};
+
+function getTxTypeByCallData(
+    calldata: string,
+    abiDecoder: any,
+): methodDetails | null {
+    try {
+        const result: DecoderResult = abiDecoder.decodeMethod(calldata);
+        if (result.name === 'multicall') {
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+            return parseMulticall(result.params);
+        }
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+        return findSupportedCallParams(result.params[0].value);
+    } catch (e) {
+        return null;
+    }
+}
+
+export function parseMulticall(params: MulticallParam[]): methodDetails | null {
+    const dataInput = params.find((param) => param.name === 'data');
+    if (!dataInput) {
+        return null;
+    }
+    if (dataInput.type === 'bytes[]') {
+        for (const v of dataInput.value) {
+            const result = findSupportedCallParams(v);
+            if (result) {
+                return result;
+            }
+        }
+    }
+    return findSupportedCallParams(dataInput.value);
+}
+
+function findSupportedCallParams(data: string): methodDetails | null {
+    const method = data.length >= 10 ? data.slice(0, 10) : '';
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    return uniswapV3MethodsMap[method] ? uniswapV3MethodsMap[method] : null;
 }
