588ac9fb72681dfb41f8ad2acd889e7467ef8860
Nick Kozlov <22479658+EnoRage@users.noreply.github.com>
2022-02-18 13:38:31 +0300

    Update normalization.ts

--- src/decoders/swap/uniswap/normalization.ts
+++ src/decoders/swap/uniswap/normalization.ts
@@ -1,8 +1,6 @@
 import { BigNumber } from '@ethersproject/bignumber';
 import { DecoderResult, MulticallParam, PermitTx, SwapTx, TxType, UnwrapTx } from './types';
-import { SwapTxDecoded, UnwrapTxDecoded } from '../../../model/swap-tx.model';
-import { findTokenByAddress } from '../../../helpers/tokens.helper';
-import { BlockchainResources } from '../../../model/common.model';
+
 import { normalizeEstimation } from './estimation';
 
 export function getTxTypeByCallData(
@@ -142,64 +140,6 @@ export function normalizeSelfPermitAllowed(data: DecoderResult): PermitTx | unde
         return undefined;
     }
 
-}
-
-export function buildSwapTxDecoded(
-    resources: BlockchainResources,
-    tx: SwapTx,
-    estimatedValue: string
-): SwapTxDecoded | undefined {
-    try {
-        const dstToken = findTokenByAddress(resources, tx.params.dstTokenAddress);
-        const srcToken = findTokenByAddress(resources, tx.params.srcTokenAddress);
-        if (!srcToken || !dstToken) {
-            return undefined;
-        }
-        if (tx.type === TxType.SWAP_INPUT) {
-            return {
-                dstAmount: BigNumber.from('0x' + estimatedValue),
-                dstToken,
-                minReturnAmount: BigNumber.from(tx.params.minReturnAmount),
-                srcAmount: BigNumber.from(tx.params.srcAmount),
-                srcToken,
-            }
-        }
-        if (tx.type === TxType.SWAP_OUTPUT) {
-            return {
-                dstToken,
-                srcToken,
-                srcAmount: BigNumber.from('0x' + estimatedValue),
-                amountInMaximum: BigNumber.from(tx.params.amountInMaximum),
-                dstAmount: BigNumber.from(tx.params.dstAmount),
-
-            }
-        }
-
-       return undefined
-    } catch (e) {
-        return undefined;
-    }
-}
-
-export function buildUnwrapTxDecoded(
-    resources: BlockchainResources,
-    tx: UnwrapTx,
-    dstAmountRaw: string,
-    dstTokenAddress: string,
-): UnwrapTxDecoded | null {
-    try {
-        const token = findTokenByAddress(resources, dstTokenAddress);
-        if (!token) {
-            return null;
-        }
-        return {
-            amount: BigNumber.from('0x' + dstAmountRaw),
-            minReturnAmount: BigNumber.from(tx.params.minReturnAmount),
-            token,
-        }
-    } catch (e) {
-        return null;
-    }
 }
 
 export function getEstimatedValue(estimated: { data: unknown, error?: Error }): string | undefined {
