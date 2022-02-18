4309acc5035a2ad35cc697583d853c97dc665170
Nick Kozlov <22479658+EnoRage@users.noreply.github.com>
2022-02-18 13:55:56 +0300

    Update multiple-tx.model.ts

--- src/model/multiple-tx.model.ts
+++ src/model/multiple-tx.model.ts
@@ -1,7 +1,8 @@
 import { SwapTxDecoded } from './swap-tx.model';
 import { UnwrapTxDecoded } from './unwrap-tx.model';
+import { ApproveTxDecoded } from './approve-tx.model';
 
 export interface MultipleTxsDecoded {
-    readonly txs: (SwapTxDecoded | UnwrapTxDecoded)[];
+    readonly txs: (SwapTxDecoded | UnwrapTxDecoded | ApproveTxDecoded)[];
     readonly error?: Error;
 }
