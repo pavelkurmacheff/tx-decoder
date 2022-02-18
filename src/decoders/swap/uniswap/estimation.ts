5e86b730410da5da8d56d2fa2ced115a1492a2e1
Nick Kozlov <22479658+EnoRage@users.noreply.github.com>
2022-02-18 16:14:32 +0300

    Update estimation.ts

--- src/decoders/swap/uniswap/estimation.ts
+++ src/decoders/swap/uniswap/estimation.ts
@@ -1,8 +1,11 @@
 export function normalizeEstimation(response: unknown): string {
     switch (typeof response) {
         case 'string':
-            if (response.length > 64) {
-                return response.substring(response.length - 64, response.length);
+            if (response.length > 128) {
+                const data = response.substring(response.length - 64, response.length);
+                if (data === '0000000000000000000000000000000000000000000000000000000000000000') {
+                    return response.substring(response.length - 128, response.length - 64);
+                }
             }
     }
     return '';
