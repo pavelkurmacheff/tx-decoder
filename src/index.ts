112327e6cdaffa50f186a7fc744e0e009d1760cc
Nick Kozlov <22479658+EnoRage@users.noreply.github.com>
2022-02-22 14:03:54 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export * from './tx-confirm-data.builder';
-export * from './oinch-tx.decoder';
+export * from './common-tx.decoder';
 export * from './model/common.model';
 export * from './model/tx-ui-items.model';
 
