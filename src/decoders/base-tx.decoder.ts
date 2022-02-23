b989b072904baa812107845f0ac731c5a593c702
Nick Kozlov <22479658+EnoRage@users.noreply.github.com>
2022-02-23 11:48:32 +0300

    Update base-tx.decoder.ts

--- src/decoders/base-tx.decoder.ts
+++ src/decoders/base-tx.decoder.ts
@@ -1,10 +1,15 @@
-import {BlockchainResources, BlockchainRpcCaller, DecodeInfo, Transaction} from '../model/common.model';
+import {
+    BlockchainRpcCaller,
+    DecodeInfo,
+    Transaction,
+    Web3Resources
+} from '../model/common.model';
 import {TransactionReceipt} from '@ethersproject/abstract-provider';
 import {DecodedTx} from '../model/decoded-tx.model';
 import { NetworkEnum } from '../const/common.const';
 
 export interface TxDecoder<T> {
-    readonly resources: BlockchainResources;
+    readonly resources: Web3Resources;
     readonly rpcCaller: BlockchainRpcCaller;
     readonly decodeInfo: DecodeInfo;
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
