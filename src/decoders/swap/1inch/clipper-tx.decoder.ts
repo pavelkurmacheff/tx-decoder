ee0aad8934898b62df77780f8822c5ddfa5ca7d0
Nick Kozlov <22479658+EnoRage@users.noreply.github.com>
2022-02-23 11:48:29 +0300

    Update clipper-tx.decoder.ts

--- src/decoders/swap/1inch/clipper-tx.decoder.ts
+++ src/decoders/swap/1inch/clipper-tx.decoder.ts
@@ -1,4 +1,9 @@
-import {BlockchainResources, BlockchainRpcCaller, DecodeInfo, Transaction} from '../../../model/common.model';
+import {
+    BlockchainRpcCaller,
+    DecodeInfo,
+    Transaction,
+    Web3Resources
+} from '../../../model/common.model';
 import {TxDecoder} from '../../base-tx.decoder';
 import {TransactionReceipt} from '@ethersproject/abstract-provider';
 import {getDestAmountViaEstimation, getReturnAmountFromLogs} from '../../../helpers/dest-amount.helper';
@@ -14,7 +19,7 @@ export interface ClipperTxItemData {
 }
 
 export class ClipperTxDecoder implements TxDecoder<ClipperTxItemData> {
-    constructor(readonly resources: BlockchainResources,
+    constructor(readonly resources: Web3Resources,
                 readonly rpcCaller: BlockchainRpcCaller,
                 readonly decodeInfo: DecodeInfo,
                 readonly txData: ClipperTxItemData,
