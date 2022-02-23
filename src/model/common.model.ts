709e399e0a088d83ef90d515bddbeaa12b23261e
Nick Kozlov <22479658+EnoRage@users.noreply.github.com>
2022-02-23 11:48:01 +0300

    Update common.model.ts

--- src/model/common.model.ts
+++ src/model/common.model.ts
@@ -1,5 +1,6 @@
 import {BigNumber} from '@ethersproject/bignumber';
 import {Interface} from '@ethersproject/abi';
+import { CustomTokensService } from '../helpers/tokens/custom-tokens.service';
 
 export interface Transaction {
     from: string;
@@ -22,6 +23,10 @@ export interface Token {
 export interface BlockchainResources {
     readonly tokens: {[key: string]: Token}, // https://tokens.1inch.io/v1.1/1
     readonly tokenPrices: {[key: string]: string}, // https://token-prices.1inch.io/v1.1/1
+}
+
+export interface Web3Resources extends BlockchainResources {
+    readonly customTokens: CustomTokensService,
 }
 
 export interface BlockchainRpcCaller {
