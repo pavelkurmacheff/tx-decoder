4e6653d68c93a339c20ad6aa0c8e50b059d688eb
Nick Kozlov <22479658+EnoRage@users.noreply.github.com>
2022-02-23 11:48:00 +0300

    Update tx-confirm-data.builder.ts

--- src/tx-confirm-data.builder.ts
+++ src/tx-confirm-data.builder.ts
@@ -1,11 +1,13 @@
 import {Item, TxType} from './model/tx-ui-items.model';
 import {BlockchainResources, BlockchainRpcCaller, Transaction} from './model/common.model';
 import {CommonTxDecoder} from './common-tx.decoder';
+import { NetworkEnum } from './const/common.const';
 
 export class TxConfirmDataBuilder {
     constructor(
         private readonly resources: BlockchainResources,
         private readonly rpcCaller: BlockchainRpcCaller,
+        private readonly chainId: NetworkEnum,
     ) {
     }
 
@@ -15,7 +17,7 @@ export class TxConfirmDataBuilder {
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
         dataArguments: any
     }> {
-        const decoder = new CommonTxDecoder(this.resources, this.rpcCaller);
+        const decoder = new CommonTxDecoder(this.resources, this.rpcCaller, this.chainId);
 
         const {data, config, dataArguments, txConfig} = await (typeof txConfigOrHash === 'string'
             ? decoder.decodeTxByLogs(txConfigOrHash)
